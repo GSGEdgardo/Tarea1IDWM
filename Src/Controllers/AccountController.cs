386b4a9dea0f93f6c2c02fe61c45a019b6a1f8e9
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-04-05 12:39:47 -0300

    feat: login method added.
    New login method addedd to the AccountController. This method validates the user's credentials and returns the user's account details if the credentials are valid.

--- Src/Controllers/AccountController.cs
+++ Src/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 using courses_dotnet_api.Src.DTOs.Account;
 using courses_dotnet_api.Src.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace courses_dotnet_api.Src.Controllers;
 
@@ -13,6 +17,46 @@ public class AccountController : BaseApiController
     {
         _userRepository = userRepository;
         _accountRepository = accountRepository;
+    }
+
+    [HttpPost("login")]
+    [Route("api/account/login")]
+    public async Task<IResult> Login(LoginDto loginDto)
+    {
+        //Dejaré un mensaje predefinido por si las credenciales no coinciden.
+        string message = "Credentials are Invalid.";
+        var user = await _accountRepository.GetUserByEmailAsync(loginDto.Email);
+        //El loginDto va a recibir los datos de la solicitud HTTP que es un Post en este caso
+        //Primero debo verificar que el usuario exista o no y para eso tenemos un método en el userRepository
+        if (user is null)
+        {
+            //Si el usuario no existe, le decimos que no se encuentra registrado mediante una bad request
+            return Results.BadRequest(message);
+        }
+        //Ahora que se validó ya que el usuario está guardado en la base de datos, necesitamos su información
+        //pero debemos ver el tema de la contraseña, como tenemos la contraseña hasheada y con una sal, hay que 
+        //replicarla, y para eso necesitamos usar el HMACSHA512 que es el que se usa en el AccountRepository para añadir la cuenta
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        // Y ahora hay que comparar el Hash obtenido en la solicitud con el de la base de datos
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Results.BadRequest(message);
+            }
+        }
+        
+        var email = loginDto.Email;
+
+        //Tecnicamente el AccountDto no debería ser null porque se validó que el correo esté en la base de datos
+        //y si está en esta parte del código significa que lo encontró, pero por si acaso se usará el '?' en el AccountDto
+        AccountDto? accountDto = await _accountRepository.GetAccountAsync(email);
+
+        //Como se pidió la idea es que si el usuario logea, se envíe el rut, el nombre, el correo y el token.
+        return Results.Ok(accountDto);
     }
 
     [HttpPost("register")]
